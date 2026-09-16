d6140c936bce2addeef095753921fdcd0fafead7
miguelvieira150 <miguel_vieira150@estudante.sesisenai.org.br>
2026-09-16 16:26:36 -0300

    exercicio 3 -parte 2

---
+++ parte2-condicionais/ex3.py
@@ -0,0 +1,7 @@
+num = float(input('escreva um numero: '))
+if num > 0:
+    print(f'o numero {num} e positivo')
+elif num < 0:
+    print (f'o numero {num} e negativo')
+else:
+    print(f'o numero {num} é 0')
